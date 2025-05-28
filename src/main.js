350422bd190a6ef8326c4c89d407689d87ac2982
Jarvis <mengda636431@outlook.com>
2025-05-28 11:20:07 +0800

    增加了naive UI和xicons作为UI和图标库

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import naive from 'naive-ui'
+
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(naive)
 app.mount('#app')
