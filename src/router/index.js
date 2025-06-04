f8ed236195013710465a30462d8970990a6b9b2c
Jarvis <mengda636431@outlook.com>
2025-06-04 20:10:53 +0800

    移除了不必要的文件，完善了每个展厅的信息配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,6 @@ const routes = [
     component: () => import('../views/Home.vue')
   },
   {
-    path: '/exhibition/:id',
-    name: 'Exhibition',
-    component: () => import('../views/Exhibition.vue')
-  },
-  {
     path: '/information/:id',
     name: 'Information',
     component: () => import('../views/Information.vue')
