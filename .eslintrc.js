472dd18ba30181fabea0b6e0dab2b7e5db8d559a
Jarvis <mengda636431@gmail.com>
2025-09-20 18:03:03 +0800

    [fea] 加入 eslint 和 prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
+};
